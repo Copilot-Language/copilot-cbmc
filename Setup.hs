d1e7f92bd6fa6735f550d3bfbb2dce02138f4f89
Lee Pike <leepike@galois.com>
2011-09-11 22:47:52 -0700

    Find CBMC in the path!

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,21 @@
-import Distribution.Simple
-main = defaultMain
+module Main ( main ) where
+
+import System.Directory    (findExecutable)
+import Distribution.Simple (defaultMainWithHooks, simpleUserHooks, postInst)
+import System.Directory    (findExecutable)
+import System.Exit         (exitWith, ExitCode(..))
+
+main :: IO ()
+main = defaultMainWithHooks simpleUserHooks{ postInst = checkDefSolver }
+ where checkDefSolver _ _ _ _ = do
+                mbP <- findExecutable "cbmc"
+                case mbP of
+                   Nothing -> do putStrLn "***"
+                                 putStrLn "*** The copilot-cbmc library requires the solver cbcm to be installed."
+                                 putStrLn "*** The executable CBMC must be in your path."
+                                 putStrLn "*** Please install CBMC and put it in your path!"
+                                 putStrLn "*** CBMC can downloaded at http://www.cprover.org/cbmc/"
+                                 putStrLn "***"
+                   Just _  -> return ()
+                exitWith ExitSuccess
+
